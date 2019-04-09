a7d3c50dd94acd227942218fd4f95ebf9d7026cc
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-09 17:30:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === '-bot') {
+    msg.reply('سرعت البوت (ms24)!');
   }
 });
 
